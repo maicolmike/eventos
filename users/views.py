c5c7753710f47c9a549295bca8ddcbccf89b61f6
Maicol <maicolmike@hotmail.com>
2026-03-24 16:44:24 -0500

    inicial

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from .forms import LoginUser  # 👈 IMPORTANTE
 from django.contrib.auth.decorators import login_required
 
+#Inicio de sesion login
 def login_view(request):
     if request.method == 'POST':
         form = LoginUser(request.POST)
@@ -15,8 +16,7 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                #messages.success(request, 'Bienvenido {}'.format(user.username))
-                messages.success(request, 'Bienvenido {}'.format(user.nombres))
+                messages.success(request, 'Bienvenido {}'.format(user.username))
                 return redirect('index')  # Redirige al usuario a la página de inicio
             else:
                 messages.error(request, 'Usuario o contraseña incorrectos')
@@ -32,7 +32,8 @@ def login_view(request):
         'form': form
     })
 
-
+#Cerrar de sesion login
 def logout_view(request):
     logout(request)
+    messages.error(request,'Sesion cerrada')
     return redirect('login')
