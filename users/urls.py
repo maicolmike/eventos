98d1c1ea7166e3c5fdaba77f01e34844d88895c3
Maicol <maicolmike@hotmail.com>
2026-03-25 13:52:43 -0500

    inicial

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('register/',views.register, name='register'),
+    path('list_users/',views.list_users, name='list_users'),
+    path('change_password/',views.change_password, name='change_password'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import LoginUser  # 👈 IMPORTANTE
+from .forms import LoginUser, RegistroUsuario,CambiarClaveForm  # 👈 IMPORTANTE
 from django.contrib.auth.decorators import login_required
+from .models import User
+from django.contrib.auth import update_session_auth_hash
 
 #Inicio de sesion login
 def login_view(request):
@@ -33,7 +35,74 @@ def login_view(request):
     })
 
 #Cerrar de sesion login
+@login_required(login_url='login') 
 def logout_view(request):
     logout(request)
     messages.error(request,'Sesion cerrada')
     return redirect('login')
+
+#Registrar usuario
+@login_required(login_url='login')    
+def register(request):
+    form = RegistroUsuario(request.POST or None)
+    
+    if request.method == 'POST' and form.is_valid():
+        user = form.save() #save () se encuentra en el archivo forms.py
+        if user:
+            if form.cleaned_data['is_superuser'] == '1': #El campo en el formulario html es 1
+                #otorgar permisos de administrador
+                user.is_staff = True
+                user.is_superuser = True
+            user.save()
+            messages.success(request, 'usuario creado')
+            return redirect('register')
+    
+    return render(request, 'users/register.html', {
+        'form': form,
+        'title': "Registro",
+        })
+
+#Listar usuarios
+@login_required(login_url='login')
+def list_users(request):
+    lista_usuarios = User.objects.all()
+    return render(request, 'users/list_users.html',{ 
+        'title': "Listado Usuarios",
+        'lista_usuarios': lista_usuarios,
+    })
+
+#cambiar clave
+@login_required(login_url='login')    
+def change_password(request):
+    
+    form = CambiarClaveForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        password_actual = form.cleaned_data['passwordActual']
+        password_nueva = form.cleaned_data['passwordNew']
+        confirmar_password = form.cleaned_data['passwordNewConfirm']
+
+        # Validar que la contraseña actual sea correcta
+        if not request.user.check_password(password_actual):
+            messages.error(request, 'La contraseña actual es incorrecta.')
+            return render(request, 'users/change_password.html', {'form': form, 'title': 'Cambiar clave'})
+        
+        # Validar que la contraseña actual sea diferente de la nueva
+        if password_actual == password_nueva:
+            messages.error(request, 'La nueva contraseña debe ser diferente de la contraseña actual.')
+            return render(request, 'users/change_password.html', {'form': form, 'title': 'Cambiar clave'})
+        
+        # Validar que la nueva contraseña y la confirmación coincidan
+        if password_nueva != confirmar_password:
+            messages.error(request, 'La nueva contraseña y confirmacion de contraseña no coinciden.')
+            return render(request, 'users/change_password.html', {'form': form, 'title': 'Cambiar clave'})
+
+        # Cambiar la contraseña del usuario
+        request.user.set_password(password_nueva)   #request.user es específico para interactuar con el usuario que ha iniciado sesión en ese momento, 
+        request.user.save()
+
+        # Actualizar la sesión del usuario para evitar cerrar sesión después de cambiar la contraseña
+        update_session_auth_hash(request, request.user)
+
+        messages.success(request, 'Contraseña cambiada exitosamente.')
+
+    return render(request, 'users/change_password.html', {'form': form, 'title': 'Cambiar clave'})
